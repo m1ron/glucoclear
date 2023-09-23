621fb568efd5b6984d8cacaa7c743046926ece91
Sergii Miroshnychenko <sergey.miron@icloud.com>
2023-09-23 10:16:46 +0300

    update

--- js/script.js
+++ js/script.js
@@ -1,20 +1,2 @@
 document.addEventListener('DOMContentLoaded', function (event) {
-  var app = document.querySelector('.app');
-  if (app) {
-    app.querySelectorAll('.app-answers a').forEach(function (a) {
-      a.addEventListener('click', function (event) {
-        event.preventDefault();
-        let active = app.querySelector('.app-step.active'),
-          next = active.nextElementSibling;
-        active.classList.remove('active');
-        setTimeout(function () {
-          active.classList.remove('visible');
-          next.classList.add('visible');
-          setTimeout(function () {
-            next.classList.add('active');
-          }, 10);
-        }, 300);
-      });
-    });
-  }
 });
